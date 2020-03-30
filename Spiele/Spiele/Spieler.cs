18b4b21ed3b7a43796a01c3e648b782d9d931097
boffing <62871930+boffing@users.noreply.github.com>
2020-03-30 09:11:04 +0200

    Update Spieler.cs

--- Spiele/Spiele/Spieler.cs
+++ Spiele/Spiele/Spieler.cs
@@ -13,12 +13,17 @@ namespace Spiele
     }
     class Spieler
     {
+        //Properties beginnen von der Konvention her mit einem Großbuchstaben
+        //_feldName nimmt man üblicherweise für das dahinterliegende Feld/Variable, sofern man sie selber implementiert
+        // bsp: 
+        // private string _spielerName //hier das Feld
+        // public string SpielerName { get {return _spielerName;} set {_spielerName = value;} } //hier die Property
         public string _spielerName { get; set; }
         public int _spielerZahl { get; set; }
         public string _spielerZeichen { get; set; }
         public bool _künstlicheIntelligenz { get; set; }
 
-
+        //Man könnte hier überlegen die Programmlogik aus dem Konstruktor in eine eigene Methode zu verlegen
         public Spieler(string spielerName, SpielerNummer Zahl)
         {
             _spielerName = spielerName;
